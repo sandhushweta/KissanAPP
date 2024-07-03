4448e9cb0d882e5afc2a9183b5aad3e23b2b2577
sandhushweta <sandhushweta1408@gmail.com>
2024-07-03 14:23:54 +0530

    packages installed

---
+++ App.js
@@ -0,0 +1,7 @@
+import {StackActions} from '@react-navigation/routers';
+import React from 'react';
+import StackNavigator from './src/screens/navigation/StackNavigator';
+const App = () => {
+  return <StackNavigator />;
+};
+export default App;
