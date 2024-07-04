80119e744f183e7a9ffc2952a1f29396a1017802
kuldeeepkumar <pro.kuldeepkumar@gmail.com>
2024-07-04 12:47:44 +0530

    Home screen design in progress

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
 import { COLORS } from "../constants/Colors";
-import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TopBar from "../components/TopBar";
 import { containerStyle } from "../styles";
 
 const Home = () => {
+  const WelcomeLogoImage = require("../../assets/img/Weather.png");
+  const ShopLogoImage = require("../../assets/img/Shop.jpeg");
+  const UserLogoImage = require("../../assets/img/User.jpeg");
+  const LightLogoImage = require("../../assets/img/light.png");
+  const DarkLogoImage = require("../../assets/img/dark.jpeg");
+
+  const [mode, setMode] = useState(false);
+
   return (
     <SafeAreaView
       style={{
@@ -29,19 +37,37 @@ const Home = () => {
             backgroundColor: "white",
             width: 150,
             height: 150,
+            justifyContent: "center",
+            alignItems: "center",
             borderTopRightRadius: 40,
             borderBottomLeftRadius: 40,
-            elevation: 5,
-          }}></View>
+            elevation: 20,
+          }}>
+          <Image
+            style={{ height: 100, width: 100 }}
+            source={WelcomeLogoImage}
+          />
+          <Text style={{ fontSize: 14, fontWeight: "700", marginTop: -21 }}>
+            Weather
+          </Text>
+        </View>
         <View
           style={{
             backgroundColor: "white",
             width: 150,
             height: 150,
+            justifyContent: "center",
+            alignItems: "center",
             borderTopLeftRadius: 40,
             borderBottomRightRadius: 40,
-            elevation: 5,
-          }}></View>
+            elevation: 20,
+          }}>
+          <Image
+            style={{ height: 65, width: 65, marginTop: 10 }}
+            source={ShopLogoImage}
+          />
+          <Text style={{ fontSize: 14, fontWeight: "700" }}>Shop</Text>
+        </View>
       </View>
       <View
         style={{
@@ -51,26 +77,43 @@ const Home = () => {
           justifyContent: "center",
           gap: 5,
           marginTop: -5,
-          elevation: 5,
+          elevation: 20,
         }}>
         <View
           style={{
             backgroundColor: "white",
             width: 150,
             height: 150,
+            justifyContent: "center",
+            alignItems: "center",
             borderTopLeftRadius: 40,
             borderBottomRightRadius: 40,
-            elevation: 5,
-          }}></View>
+            elevation: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <Image
+            style={{ height: 70, width: 70 }}
+            source={mode ? LightLogoImage : DarkLogoImage}
+          />
+          <Text style={{ fontSize: 14, fontWeight: "700" }}>Mode</Text>
+        </View>
         <View
           style={{
             backgroundColor: "white",
             width: 150,
             height: 150,
+            justifyContent: "center",
+            alignItems: "center",
             borderTopRightRadius: 40,
             borderBottomLeftRadius: 40,
-            elevation: 5,
-          }}></View>
+            elevation: 20,
+          }}>
+          <Image style={{ height: 80, width: 80 }} source={UserLogoImage} />
+          <Text style={{ fontSize: 14, fontWeight: "700", marginBottom: 15 }}>
+            Profile
+          </Text>
+        </View>
       </View>
       <View
         style={{
@@ -78,7 +121,7 @@ const Home = () => {
           width: 60,
           height: 60,
           borderRadius: 100,
-          marginLeft: 200,
+          marginLeft: 150,
           marginTop: -200,
         }}></View>
 
@@ -93,7 +136,7 @@ const Home = () => {
         <View
           style={{
             backgroundColor: COLORS.HOME_CARD_COLOR,
-            width: 200,
+            width: 155,
             height: "100%",
             borderTopRightRadius: 40,
           }}></View>
@@ -110,7 +153,7 @@ const Home = () => {
         <View
           style={{
             backgroundColor: COLORS.HOME_CARD_COLOR,
-            width: 200,
+            width: 155,
             height: "100%",
             borderTopLeftRadius: 40,
           }}></View>
